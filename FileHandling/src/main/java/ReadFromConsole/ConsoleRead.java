883a358a3d5c6a6f4d2419b6971c3aa37cdd1d47
Alaa Nabil <60174664+alaa-ai@users.noreply.github.com>
2021-06-25 06:58:12 +0200

    Update ConsoleRead.java

--- FileHandling/src/main/java/ReadFromConsole/ConsoleRead.java
+++ FileHandling/src/main/java/ReadFromConsole/ConsoleRead.java
@@ -5,12 +5,50 @@
  */
 package ReadFromConsole;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Writer;
+
+
 /**
  *
  * @author gt
  */
 public class ConsoleRead {
-    public static void main(String[] args){
-        System.out.println("Hello Java");
+
+    
+    public static void main(String[] args) throws IOException{
+         try{
+            
+            InputStreamReader r = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(r);  
+            
+            Writer w = new FileWriter("D:\\1st-AI- ITI\\Javz\\Day3\\Tasks\\ReadCons.txt");
+            BufferedWriter buffer =new BufferedWriter(w);
+            
+            String name = " ";
+            while (!name.equals("stop")){
+                try{
+                    System.out.println("Enter Data: ");
+                    name = br.readLine();
+                    System.out.println("Data is: " + name);
+                     buffer.write(name+"\n");
+                    
+                }catch (IOException ex){
+                    ex.printStackTrace();
+                }
+                
+        }
+           
+            buffer.close();
+            br.close();
+            r.close();
+        }catch (IOException ex){
+                    ex.printStackTrace();
+                }
     }
 }
+
