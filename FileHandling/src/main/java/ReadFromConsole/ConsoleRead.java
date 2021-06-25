4eca11bebb4a7a921473e5daf454417b5c8b0e5c
Alaa Nabil <alaanabel45@gmail.com>
2021-06-25 06:23:32 +0200

    ,,Hello

---
+++ FileHandling/src/main/java/ReadFromConsole/ConsoleRead.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ReadFromConsole;
+
+/**
+ *
+ * @author gt
+ */
+public class ConsoleRead {
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+    }
+}
